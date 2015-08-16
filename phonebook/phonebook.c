56f4ff3e71d3ebecff2b827b6d7c2601cc8128a6
charles620016 <charles620016@gmail.com>
2015-08-17 00:54:44 +0800

    add two optimal version of phonebook
    Add two other optimal verison of phonebook
    First one is main_optimal.c. Using smaller struct to store the last name.
    Second one is main_hash.c. Using hash table to speed up the searching.
    
    All detailed implementation is in phonebook.c

--- phonebook/phonebook.c
+++ phonebook/phonebook.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "phonebook.h"
 
-PhoneBook *findName(char last[], PhoneBook *pHead)
+/* original version */
+entry *findName(char lastname[], entry *pHead)
 {
     while (pHead != NULL) {
-        if (strcasecmp(last, pHead->lastName) == 0){
+        if (strcasecmp(lastname, pHead->lastName) == 0){
             printf("%s\n", "found it!");
             return pHead;
         }
@@ -16,13 +18,131 @@ PhoneBook *findName(char last[], PhoneBook *pHead)
     return NULL;
 }
 
-PhoneBook *append(char *lastName, PhoneBook *entry)
+entry *append(char lastName[], entry *e)
 {
-    /* Allocate memory for the new entry and put lastName in it.*/
-    entry->pNext = (PhoneBook *) malloc(sizeof(PhoneBook));
-    entry = entry->pNext;
-    strcpy(entry->lastName, lastName);
-    entry->pNext = NULL;
+    /* allocate memory for the new entry and put lastName in it.*/
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
 
-    return entry;
+    return e;
+}
+
+
+/* optimal version 1 */
+lastNameEntry *findNameOpt(char lastName[], lastNameEntry *pHead)
+{
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0){
+            printf("%s\n", "found it!");
+            return pHead;
+        }
+        pHead = pHead->pNext;
+    }
+    printf("%s\n", "not found");
+    return NULL;
+}
+
+lastNameEntry *appendOpt(char lastName[], lastNameEntry *lne)
+{
+    /* allocate memory for the new entry and put lastName in it.*/
+    lne->pNext = (lastNameEntry *) malloc(sizeof(lastNameEntry));
+    lne = lne->pNext;
+    strcpy(lne->lastName, lastName);
+    lne->pNext = NULL;
+
+    /* point to the detail information*/
+    lne->detail = (entry *) malloc(sizeof(entry));
+    strcpy(lne->detail->lastName, lastName);
+    return lne;
+}
+
+
+/* optimal version 2 */
+hashTable *createHashTable(int tableSize)
+{
+    hashTable *ht = NULL;
+    int i;
+
+    if( tableSize < 1 ){
+        return NULL;
+    }
+
+    /* Allocate the table itself. */
+    if((ht = (hashTable *) malloc(sizeof(hashTable))) == NULL) {
+        return NULL;
+    }
+
+    /* Allocate array of list. */
+    if( (ht->list = (entry **) malloc(sizeof(entry *)*tableSize)) == NULL ) {
+        return NULL;
+    }
+
+    /* Allocate list header */
+    for( i = 0; i < tableSize; i++ ) {
+        ht->list[i] = NULL;
+    }
+
+    ht->tableSize = tableSize;
+
+    return ht;
+}
+
+entry* findNameHash(char *key, hashTable *ht)
+{
+    entry *e;
+    hashIndex index = hash3(key, ht);
+    for(e = ht->list[index]; e != NULL; e = e->pNext){
+        if ( strcasecmp(key, e->lastName) == 0 ){
+            printf("%s\n", "found it!");
+            return e;
+        }
+    }
+    printf("%s\n", "not found");
+    return NULL;
+}
+
+int appendHash(char *key, hashTable *ht)
+{
+    hashIndex index = hash3(key, ht);
+    entry *newEntry;
+    // entry *currentEntry;
+
+    /* Does entry already exist? If it does, don't append it again. 
+    if( (currentEntry = findNameHash(key, ht)) != NULL ){
+        return 1;
+    }*/
+
+    /* attempt to allocate memory for entry */
+    if ((newEntry = (entry *) malloc(sizeof(entry))) == NULL){
+        return 2;
+    }
+
+    /* Insert into list */
+    strcpy(newEntry->lastName, key);
+    newEntry->pNext = ht->list[index];
+    ht->list[index] = newEntry;
+
+    return 0;
+}
+
+hashIndex hash2(char *key, hashTable *ht)
+{   
+    unsigned int hashVal = 0;
+    while(*key != '\0'){
+        hashVal+= *key++;
+    }
+
+    return hashVal % ht->tableSize;
+}
+
+hashIndex hash3(char *key, hashTable *ht)
+{
+    unsigned int hashVal = 0;
+    while(*key != '\0'){
+        hashVal = (hashVal << 5) + *key++;
+    }
+
+    return hashVal % ht->tableSize;
 }
--- phonebook/phonebook.h
+++ phonebook/phonebook.h
@@ -3,6 +3,7 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
+/* original version */
 typedef struct __PHONE_BOOK_ENTRY {
     char lastName[MAX_LAST_NAME_SIZE];
     char firstName[16];
@@ -15,9 +16,35 @@ typedef struct __PHONE_BOOK_ENTRY {
     char state[2];
     char zip[5];
     struct __PHONE_BOOK_ENTRY *pNext;
-} PhoneBook;
+} entry;
 
-PhoneBook *findName(char last[], PhoneBook *pHead);
-PhoneBook *append(char *lastName, PhoneBook *entry);
+entry *findName(char lastname[], entry *pHead);
+entry *append(char lastName[], entry *e);
+
+
+/* optimal version 1*/
+typedef struct __LAST_NAME_ENTRY{
+    char lastName[MAX_LAST_NAME_SIZE];
+    entry *detail;
+    struct __LAST_NAME_ENTRY *pNext;
+} lastNameEntry;
+
+lastNameEntry *findNameOpt(char lastname[], lastNameEntry *pHead);
+lastNameEntry *appendOpt(char lastName[], lastNameEntry *lne);
+
+
+/* optimal versoin 2*/
+typedef unsigned int hashIndex;
+typedef struct __PHONE_BOOK_HASH_TABLE {
+    unsigned int tableSize;  /* the size of the table */
+    entry **list;  /* the table elements */
+} hashTable;
+
+hashTable *createHashTable(int tableSize);
+entry* findNameHash(char *key, hashTable *ht);
+int appendHash(char *key, hashTable *ht);
+hashIndex hash1(char *key);
+hashIndex hash2(char *key, hashTable *ht);
+hashIndex hash3(char *key, hashTable *ht); 
 
 #endif
